2c53a0578fd1255f810d35069442d11d2fefc82f
Belén Pruvost <Beln Pruvost>
2016-05-11 00:49:26 -0300

    First commit

---
+++ platforms/browser/www/js/index.js
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.getElementById('btnScan').addEventListener('click', this.scan, false);
+    },
+
+    scan: function() {
+        console.log('scanning');
+            cordova.plugins.barcodeScanner.scan(
+                function (result) {
+                    window.open(result.text,'_self', 'location=no') //Opens URL in browser
+                  //alert("We got a barcode\n" +
+                  //      "Result: " + result.text + "\n" +
+                  //      "Format: " + result.format + "\n" +
+                  //      "Cancelled: " + result.cancelled);
+              }, 
+              function (error) {
+                  alert("Scanning failed: " + error);
+              }
+            );
+           
+    }    
+};
+
+function hasCameraPermission() {
+    cordova.plugins.barcodeScanner.hasCameraPermission().then(
+      function(result) {
+        // if this is 'false' you probably want to call 'requestCameraPermission' now
+        alert(result);
+      }
+    )
+  }
+
+  function requestCameraPermission(){
+    alert('antes requestCameraPermission');
+    // no callbacks required as this opens a popup which returns async
+    cordova.plugins.barcodeScanner.requestCameraPermission();
+    alert('paso requestCameraPermission');
+  }    
